092d08bcf1f0bceaaca86efb18e53cc261d75311
Tymoteusz Bak <t.bak@i-systems.pl>
2021-04-03 17:01:15 +0200

    fetch jokes from api and add unlock-tries counter

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,38 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
-interface Props {
+interface PropsSpinLock {
   digit: number;
   onChange: (digit: number) => void;
 }
 
+interface PropsReveal {
+  isOpen: boolean;
+}
+
+function Reveal({ isOpen }: PropsReveal) {
+
+  const [joke, setJoke] = useState('');
+
+  useEffect(() => {
+    fetch('https://icanhazdadjoke.com/', {
+      headers: {
+        'Accept': 'application/json'
+      }
+    }).then(res => res.json())
+      .then(res => res && res.joke)
+      .then(joke => setJoke(joke));
+  }, []);
+
+  return (
+    <div>
+      { isOpen && <p className="joke">{joke}</p> }
+    </div>
+  )
+}
+
 // const SpinLock: React.FC<Props> = ({ digit, onChange }) => {
-function SpinLock({ digit, onChange }: Props) {
+function SpinLock({ digit, onChange }: PropsSpinLock) {
   const incrementDigit = (digit + 1) % 10;
   const decrementDigit = digit > 0 ? digit - 1 : 9;
   return (
@@ -23,6 +48,7 @@ function App() {
   const password = [1,2,3];
   const [digits, setDigits] = useState([0,0,0]);
   const [isUnlocked, setIsUnlocked] = useState(false);
+  const [numTries, setNumTries] = useState(0);
 
   const setDigitAtIndex = (digit: number, idx: number) => {
     setDigits((currentDigits) => [
@@ -35,6 +61,7 @@ function App() {
   const checkPassword = () => {
     for (let i = 0; i < password.length; i++) {
       if (password[i] !== digits[i]) {
+        setNumTries((currentNumTries) => currentNumTries + 1);
         return;
       }
     }
@@ -50,8 +77,9 @@ function App() {
         }
       </div>
       <button onClick={() => checkPassword()}>Press me to open</button>
-      { !isUnlocked && <p>Sry, wrong code.</p> }
-      { isUnlocked && <p>Unlocked!</p> }
+      { !isUnlocked && numTries > 0 && <p>You've tried to unlock the jokebox <b>{numTries}</b> times.</p>}
+      { isUnlocked && <p>Jokebox unlocked! Here is your joke:</p>}
+      <Reveal isOpen={isUnlocked} />
     </div>
   );
 }
