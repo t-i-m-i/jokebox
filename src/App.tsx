fbf70673385732f20efeb9f956b81064950f09a2
Tymoteusz Bak <t.bak@i-systems.pl>
2021-04-03 16:17:45 +0200

    add spin lock component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,24 @@
 import React, { useState } from 'react';
 import './App.css';
 
+interface Props {
+  digit: number;
+  onChange: (digit: number) => void;
+}
+
+// const SpinLock: React.FC<Props> = ({ digit, onChange }) => {
+function SpinLock({ digit, onChange }: Props) {
+  const incrementDigit = (digit + 1) % 10;
+  const decrementDigit = digit > 0 ? digit - 1 : 0;
+  return (
+    <div className="control">
+      <button onClick={() => onChange(incrementDigit)}>+</button>
+      { digit }
+      <button onClick={() => onChange(decrementDigit)}>-</button>
+    </div>
+  )
+}
+
 function App() {
   const password = [1,2,3];
   const [digits, setDigits] = useState([0,0,0]);
@@ -37,24 +55,9 @@ function App() {
     <div>
       <h1>Unlockme please 🙏🏻 to get the joke 😁</h1>
       <div className="doorlock">
-        <div className="control">
-          <button onClick={() => increment(0)}>+</button>
-          { digits[0] }
-          <button onClick={() => decrement(0)}>-</button>
-        </div>
-        <div className="control">
-          <button onClick={() => increment(1)}>+</button>
-          { digits[1] }
-          <button onClick={() => decrement(1)}>-</button>
-        </div>
-        <div className="control">
-          <button onClick={() => increment(2)}>+</button>
-          { digits[2] }
-          <button onClick={() => decrement(2)}>-</button>
-        </div>
-        {/* <input type="number" value={digits[0]} onChange={(event) => setDigitAtIndex(parseInt(event.target.value), 0)} />
-        <input type="number" value={digits[1]} onChange={(event) => setDigitAtIndex(parseInt(event.target.value), 1)} />
-        <input type="number" value={digits[2]} onChange={(event) => setDigitAtIndex(parseInt(event.target.value), 2)} /> */}
+        <SpinLock digit={digits[0]} onChange={(newDigit: number) => setDigitAtIndex(newDigit, 0)} />
+        <SpinLock digit={digits[1]} onChange={(newDigit: number) => setDigitAtIndex(newDigit, 1)} />
+        <SpinLock digit={digits[2]} onChange={(newDigit: number) => setDigitAtIndex(newDigit, 2)} />
       </div>
       <button onClick={() => checkPassword()}>Press me to open</button>
       { !isUnlocked && <p>Sry, wrong code.</p> }
